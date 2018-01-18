d69404a96d48400ec3129fb27b527900916d94e1
Tom Mairs <tom.mairs@messagesystems.com>
2018-01-17 16:13:50 -0800

    Create genkey.php

---
+++ genkey.php
@@ -0,0 +1,11 @@
+<?php
+// Provide API access to a generation tool that provides a UUID for ViewInBrowser
+// Take in the CampaignID and email address
+// Use the current timestamp and calculate a UUID
+// Return the UUID that can be included in the SparkPost template
+// Variable declarations:
+// Set the root URL for viewInBrowser link. Default is THIS FQDN 
+// $URL = "";
+
+
+?>
