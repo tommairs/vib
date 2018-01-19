953cbb4fa8c2991bb73ba9b798fe1a5654a8f223
Tom Mairs <tom.mairs@messagesystems.com>
2018-01-18 22:06:45 -0800

    Update econsumer.php

--- econsumer.php
+++ econsumer.php
@@ -1,6 +1,20 @@
 
 <?php
 // Read email to a var
+// Capture inbound HTTP REQUEST
+  $verb = $_SERVER['REQUEST_METHOD'];
+  if ($verb == "POST") {
+    $body = file_get_contents("php://input");
+    $email = preg_grep("/email[\:\"\s]*[\w\d\.-_]*\@[\w\d\.-_]*/", $body); 
+    $campaign_id = preg_grep("/campaign_id[\:\"\s]*[\w\d\.-_]*/", $body);
+    $t = date("Y-m-d:h:i:s",time()));
+    $options = [
+      'cost' => 11,
+      'salt' => mcrypt_create_iv(22, MCRYPT_DEV_URANDOM),
+    ];
+    $UUID = password_hash($email.$campaign_id.$t, PASSWORD_BCRYPT, $options);
+    echo $proto.$URL."?l="$UUID;
+  }
 
 // Extract HTML part and save it
 
