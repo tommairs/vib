d0b09ceafb85ecef76a34adfcdb70a6d2789d3bc
Tom Mairs <tom.mairs@messagesystems.com>
2018-01-23 14:44:00 -0700

    Update econsumer.php

--- econsumer.php
+++ econsumer.php
@@ -1,25 +1,35 @@
-
 <?php
 // Read email to a var
 // Capture inbound HTTP REQUEST
 $verb = $_SERVER['REQUEST_METHOD'];
 $authtoken = $_SERVER['HTTP_X-MessageSystems-Webhook-Token'];
-$filepath = "./data/";
+$filepath = "/var/www/html/seapps/vib/data/";
+$urlpath = "http://app.trymsys.net/seapps/vib/data/";
 if ($verb == "POST") {
-  if ($authtoken="5l<redacted>69"){
+  if ($authtoken="<redacted>"){
     $body = file_get_contents("php://input");
-    $result = json_decode($body);
-    // Extract HTML part and save it
-    $location = "123123123123123";
-    $htmlpart = $result['relay_message']['html'];
-    file_put_contents ($filepath.$location,$htmlpart);
+    $result = json_decode($body,TRUE);
+    
+    // Extract target location and save it
+    $rawmessage.= $result[0]['msys']['relay_message']['content']['html'];
+    preg_match('/http(.*)\/\/(.*)l=([a-zA-Z\d$_-]*).html/',$rawmessage,$viblink);
+    $location = $viblink[3];
 
-    // Extract TEXT part and save it
-    $textpart = $result['relay_message']['text'];
-    file_put_contents ($filepath.$location."txt",$textpart);
+    // Add link to TEXT version
+    $htmlpart = "<html><body><p><center>";
+    $htmlpart .= "<a href=\"".$urlpath.$location."-txt.html\">Click here for any available TEXT version</a></center><br>";
+    $htmlpart .= $result[0]['msys']['relay_message']['content']['html'];
+    $htmlpart .= "</body></html>";
 
-  }  
+    // Add link to HTML version
+    $textpart = "<html><body><p><center>";
+    $textpart .= "<a href=\"".$urlpath.$location.".html\">Click here for any available HTML version</a></center><br><pre>";
+    $textpart .= $result[0]['msys']['relay_message']['content']['text'];
+    $textpart .= "</pre></body></html>";
+
+    file_put_contents ($filepath.$location.".html",$htmlpart);
+    file_put_contents ($filepath.$location."-txt.html",$textpart);
+  }
 }
-
 
 ?>
